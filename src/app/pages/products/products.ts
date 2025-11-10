2de939133d74e09f8da2b3e94b048f1609cefc6d
kmkaze <kmilo.guitar27@gmail.com>
2025-11-09 19:41:08 -0500

    Página de Games finalizada

--- src/app/pages/products/products.ts
+++ src/app/pages/products/products.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductCard } from '../../components/product-card/product-card'; // ✅ ruta correcta
+
+interface Product {
+  name: string;
+  image: string;
+  category: string;
+  price: number;
+}
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ProductCard], // ✅ importado aquí
   templateUrl: './products.html',
-  styleUrl: './products.css',
+  styleUrls: ['./products.css']
 })
 export class Products {
+  products: Product[] = [
+    {
+      name: 'Guitarra Eléctrica Gibson Lp Studio Cherry Sunburst',
+      image: 'assets/img/Guitarras-electricas/LPST01HSCH1_front_76ecc6d2-9260-4c42-8dd1-8edfcb278346.webp',
+      category: 'Guitarras',
+      price: 4500000
+    },
+    {
+      name: 'Batería Electrónica Roland TD-1DMK',
+      image: 'assets/img/Baterias/roland.webp',
+      category: 'Baterías',
+      price: 5200000
+    }
+  ];
 
+  selectedCategory: string = 'Todos';
+
+  get filteredProducts() {
+    if (this.selectedCategory === 'Todos') return this.products;
+    return this.products.filter(p => p.category === this.selectedCategory);
+  }
+
+  changeCategory(cat: string) {
+    this.selectedCategory = cat;
+  }
 }
